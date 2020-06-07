6f04f6fe8944a149bdc3975d53e5b7cb00ad693b
RamenDNoodle <51265774+ramendnoodle@users.noreply.github.com>
2020-06-07 20:02:55 +0200

    Ajout de boostrap en fichier local, début de construction du dashboard admin

---
+++ src/admin/template/dashboard.php
@@ -0,0 +1,73 @@
+<div class="row min-vh-100">
+
+    <div class="col-2 min-vh-100 sticky-top">
+        <div class="row">
+            <div class="col-12 text-center bg-info" style="height: 50px;">
+                Ramen Content Manager
+            </div>
+            <div class="col-12 pl-3">
+                <div class="row h-100" style="background-color: rgba(23,162,184,0.6)">
+
+                    <div class="col-12 py-1" style="border-bottom: 2px black dashed;border-top: solid 2px black;background-color: rgba(23,162,184,0.7);">
+                        <div class="row justify-content-between align-items-center">
+                            <div class="col-4">
+                                <span class="h6">Pages</span>
+                            </div>
+                            <div class="col-4 text-right">
+                                <a href="#!" class="btn btn-success"><i class="far fa-plus-square"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 py-1">
+                        <a href="#!"><small class="text-muted">Page 1</small></a>
+                    </div>
+                    <div class="col-12 py-1">
+                        <a href="#!"><small class="text-muted">Page 2</small></a>
+                    </div>
+                    <div class="col-12 py-1">
+                        <a href="#!"><small class="text-muted">Page 3</small></a>
+                    </div>
+
+                    <div class="col-12 py-1" style="border-bottom: dashed 2px black;border-top: solid 2px black;background-color: rgba(23,162,184,0.7);">
+                        <div class="row justify-content-between align-items-center">
+                            <div class="col-4">
+                                <span class="h6">Éléments</span>
+                            </div>
+                            <div class="col-4 text-right">
+                                <a href="#!" class="btn btn-success"><i class="far fa-plus-square"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 py-1">
+                        <a href="#!"><small class="text-muted">Headers</small></a>
+                    </div>
+                    <div class="col-12 py-1">
+                        <a href="#!"><small class="text-muted">Footers</small></a>
+                    </div>
+                    <div class="col-12 py-1">
+                        <a href="#!"><small class="text-muted">Section Réseaux Sociaux</small></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-10 min-vh-100">
+
+        <div class="row">
+
+            <div class="col-12 bg-info" style="height: 50px">
+            </div>
+
+            <div class="col-12 bg-light min-vh-100">
+
+                Content
+                <?php // $content ?>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
---
+++ src/admin/template/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+
+    <title>Ramen Content Manager ~ Admin Side</title>
+
+    <link rel="stylesheet" href="../css/bootstrap.css"/>
+    <link rel="stylesheet" href="../css/style.css"/>
+    <script src="https://kit.fontawesome.com/363dc41312.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
