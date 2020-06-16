1b09636ea5fb99d2e3f27e479b22fe4ab5580c69
Gauthier Chambin <chambingauthier@gmail.com>
2020-06-16 18:22:42 +0200

    Ptites améliorations

--- src/admin/template/dashboard.php
+++ src/admin/template/dashboard.php
@@ -1,6 +1,6 @@
-<div class="row min-vh-100">
+<div class="row">
 
-    <div class="col-2 min-vh-100 sticky-top">
+    <div class="col-2">
         <div class="row">
             <div class="col-12 text-center bg-info" style="height: 50px;">
                 Ramen Content Manager
@@ -9,57 +9,74 @@
                 <div class="row h-100" style="background-color: rgba(23,162,184,0.6)">
 
 
-                    <div class="col-12 py-1"
-                         style="border-bottom: 2px black dashed;border-top: solid 2px black;background-color: rgba(23,162,184,0.7);">
-                        <div class="row justify-content-between align-items-center">
-                            <div class="col-4">
-                                <span class="h6">Pages</span>
-                            </div>
-                            <div class="col-4 text-right">
-                                <a href="#!" class="btn btn-success"><i class="far fa-plus-square"></i></a>
+                        <div class="col-12 py-1" style="border-top: solid 2px black;background-color: rgba(23,162,184,0.7);">
+                            <div class="row align-items-center">
+                                <div class="col-4">
+                                    <span class="h6">Pages</span>
+                                </div>
+                                <div class="col-4 offset-3">
+                                    <div class="btn-group" role="group" aria-label="">
+                                        <a href="#!" class="btn btn-success"><i class="far fa-plus-square"></i></a>
+                                        <a class="btn btn-secondary" data-toggle="collapse" href="#collapsePage" role="button" aria-expanded="true" aria-controls="collapsePage"><i class="fas fa-bars"></i></a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-12 py-1">
-                        <a href="#!"><small class="text-muted">Page 1</small></a>
-                    </div>
-                    <div class="col-12 py-1">
-                        <a href="#!"><small class="text-muted">Page 2</small></a>
-                    </div>
-                    <div class="col-12 py-1">
-                        <a href="#!"><small class="text-muted">Page 3</small></a>
-                    </div>
 
-                    <div class="col-12 py-1"
-                         style="border-bottom: dashed 2px black;border-top: solid 2px black;background-color: rgba(23,162,184,0.7);">
-                        <div class="row justify-content-between align-items-center">
+
+                    <div class="collapse show" id="collapsePage">
+                        <div class="col-12 py-1">
+                            <a href="#!"><small class="text-muted">Page 1</small></a>
+                        </div>
+                        <div class="col-12 py-1">
+                            <a href="#!"><small class="text-muted">Page 2</small></a>
+                        </div>
+                        <div class="col-12 py-1">
+                            <a href="#!"><small class="text-muted">Page 3</small></a>
+                        </div>
+                    </div>
+                    <div class="col-12 py-1" style="border-top: solid 2px black;background-color: rgba(23,162,184,0.7);">
+                        <div class="row align-items-center">
                             <div class="col-4">
                                 <span class="h6">Éléments</span>
                             </div>
-                            <div class="col-4 text-right">
-                                <a href="#!" class="btn btn-success"><i class="far fa-plus-square"></i></a>
+                            <div class="col-4 offset-3">
+                                <div class="btn-group" role="group" aria-label="">
+                                    <a href="#!" class="btn btn-success"><i class="far fa-plus-square"></i></a>
+                                    <a class="btn btn-secondary" data-toggle="collapse" href="#collapseElement" role="button" aria-expanded="true" aria-controls="collapseElement"><i class="fas fa-bars"></i></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 py-1">
-                        <a href="#!"><small class="text-muted">Headers</small></a>
-                    </div>
-                    <div class="col-12 py-1">
-                        <a href="#!"><small class="text-muted">Footers</small></a>
-                    </div>
-                    <div class="col-12 py-1">
-                        <a href="#!"><small class="text-muted">Section Réseaux Sociaux</small></a>
+                    <div class="collapse show" id="collapseElement">
+                        <div class="col-12 py-1">
+                            <a href="#!"><small class="text-muted">Headers</small></a>
+                        </div>
+                        <div class="col-12 py-1">
+                            <a href="#!"><small class="text-muted">Footers</small></a>
+                        </div>
+                        <div class="col-12 py-1">
+                            <a href="#!"><small class="text-muted">Section Réseaux Sociaux</small></a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="col-10 min-vh-100">
+    <div class="col-10">
 
         <div class="row">
 
             <div class="col-12 bg-info" style="height: 50px">
+                <div class="row">
+                    <div class="col-2 offset-10">
+                        <div class="btn-group btn-group-lg" role="group" aria-label="">
+                            <a href="#!" class="btn btn-dark" data-toggle="tooltip" data-placement="bottom" title="Gestion des Utilisateurs"><i class="fas fa-users-cog"></i></a>
+                            <a href="#!" class="btn btn-danger" data-toggle="tooltip" data-placement="bottom" title="Déconexion"><i class="fas fa-power-off"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="col-12 bg-light min-vh-100">
--- src/admin/template/footer.php
+++ src/admin/template/footer.php
@@ -1,6 +1,16 @@
 
-
+<div class="row w-100">
+    <div class="col-4">
+        Déveloper par <a href="https://gauthierchambin.fr" target="_blank">Gauthier Chambin</a> et <a href="https://github.com/PaulineTeoulle" target="_blank">Pauline Teoulle</a>.
+    </div>
+</div>
 <script src="../js/jquery.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="../js/bootstrap.js"></script>
+<script type="text/javascript">
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
 </body>
 </html>
